3b22e059da6bfc76e7a47146bdeace38470ba2ac
BaronEffeh <effehbaron@gmail.com>
2022-09-13 08:08:00 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * print_aplphabet - print alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char latter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
